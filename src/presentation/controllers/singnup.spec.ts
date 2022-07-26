21deb3d4e9f64584de915a913aa1be76e5c2c59b
amaro <j.amarorn@gmail.com>
2022-07-26 00:12:00 -0300

    test: jest Should return 400 if no email is provided

--- src/presentation/controllers/singnup.spec.ts
+++ src/presentation/controllers/singnup.spec.ts
@@ -14,4 +14,18 @@ describe('Singnup Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Name is required'))
   })
+
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SingupController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('email is required'))
+  })
 })
