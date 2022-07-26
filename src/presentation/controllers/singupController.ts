7f84dcb7a74e5df1389dd3d1661eb6b2986ca7df
amaro <j.amarorn@gmail.com>
2022-07-26 00:10:59 -0300

    feat: ensure SingUpCotroller return an erro r if no email is provided

--- src/presentation/controllers/singupController.ts
+++ src/presentation/controllers/singupController.ts
@@ -1,8 +1,16 @@
 export class SingupController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Name is required')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Name is required')
+      }
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('email is required')
+      }
     }
   }
 }
